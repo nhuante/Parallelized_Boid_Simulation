07157df5aac1f8492934c823a2f8edfdb99b7565
nhuante <huante@chapman.edu>
2025-12-07 13:49:42 -0800

    mid-first build, moving to new directory locally, need to push current changes

---
+++ build/CMakeFiles/BoidsSim.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "BoidsSim.exe"
+  "BoidsSim.exe.manifest"
+  "BoidsSim.pdb"
+  "CMakeFiles/BoidsSim.dir/src/main.cpp.obj"
+  "CMakeFiles/BoidsSim.dir/src/main.cpp.obj.d"
+  "CMakeFiles/BoidsSim.dir/src/renderer.cpp.obj"
+  "CMakeFiles/BoidsSim.dir/src/renderer.cpp.obj.d"
+  "CMakeFiles/BoidsSim.dir/src/simulation.cpp.obj"
+  "CMakeFiles/BoidsSim.dir/src/simulation.cpp.obj.d"
+  "libBoidsSim.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoidsSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
